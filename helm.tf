be6b10dd710be2e416c154233274e9c5132faf50
Sopio <Berishvilisopio@gmail.com>
2022-07-31 22:53:14 +0400

    Add Helm resource

---
+++ helm.tf
@@ -0,0 +1,34 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+
+  values = [
+    "${file("jenkins-values.yaml")}"
+  ]
+
+  set_sensitive {
+    name  = "controller.adminUser"
+    value = ""
+  }
+  set_sensitive {
+    name = "controller.adminPassword"
+    value = ""
+  }
+  set_sensitive {
+    name = "adminPassword"
+    value = ""
+  }
+}
