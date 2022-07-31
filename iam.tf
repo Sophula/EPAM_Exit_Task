5cf20b2c442e999c8c9010838c85e9f9f8e5f862
Sopio <Berishvilisopio@gmail.com>
2022-07-31 21:53:16 +0400

    Configure Roles

--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role" "eks-iam-role" {
-  name = "eks-iam-role"
+/*resource "aws_iam_role" "eks-iam-role" {
+ name = "eks-iam-role"
 
-  path = "/"
+ path = "/"
 
-  assume_role_policy = <<EOF
+ assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -20,11 +20,11 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-iam-role.name
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
 }
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-iam-role.name
-}
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
+}*/
 
