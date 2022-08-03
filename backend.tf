ead5ef1e51453666b7e30ea92961bf5a7d7f5b37
Sopio <Berishvilisopio@gmail.com>
2022-08-03 15:05:01 +0400

    kinda nginx-ingress

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "exittaskepam"
+    bucket  = "exittaskepam22"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
--- main.tf
+++ main.tf
@@ -25,6 +25,10 @@ module "vpc" {
   }
 }
 
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
@@ -166,4 +170,5 @@ resource "aws_security_group" "allow_ssh_to_bastion" {
     security_groups = [aws_security_group.allow_ssh_to_bastion.id]
   }
 }*/
+
 
