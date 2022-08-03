5147a465f74dca65008490945f287cba4de91143
Sopio <Berishvilisopio@gmail.com>
2022-08-03 15:08:45 +0400

    kind ingress

--- helm.tf
+++ helm.tf
@@ -24,11 +24,35 @@ resource "helm_release" "jenkins" {
     value = ""
   }
   set_sensitive {
-    name = "controller.adminPassword"
+    name  = "controller.adminPassword"
     value = ""
   }
   set_sensitive {
-    name = "adminPassword"
+    name  = "adminPassword"
     value = ""
   }
+}
+
+resource "helm_release" "ingress-nginx" {
+  name = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  namespace = "ingress"
+  version = "4.0.13"
+  create_namespace = true
+  timeout = 300
+
+  values = [
+    "${file("chart_values.yml")}"
+  ]
+
+  set {
+    name  = "cluster.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "metrics.enabled"
+    value = "true"
+  }  
 }
